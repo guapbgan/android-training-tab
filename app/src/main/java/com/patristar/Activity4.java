0876aecc53f6668c9a556974e7ed86dd3e5c18f3
Anthony <skou3917@gmail.com>
2020-09-23 17:15:29 +0800

    2020/09/23 tab

---
+++ app/src/main/java/com/patristar/Activity4.java
@@ -0,0 +1,12 @@
+package com.patristar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity4 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout4);
+    }
+}
